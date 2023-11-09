c37b33b60b4ec626f0880eb170b5c412a250f550
Your Name <kharalmukunda2@gmail.com>
2023-11-09 09:52:49 +0545

    feat: delete

--- src/pages/admin/Products/index.tsx
+++ src/pages/admin/Products/index.tsx
@@ -1,13 +1,23 @@
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import {  useEffect, useState } from 'react';
-import { getData } from '../../../services/axios.service';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
+import { getData } from "../../../services/axios.service";
+import Loader from "../../../components/Loader";
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import { Button } from "react-bootstrap";
+import moment from "moment";
+import axios from "axios";
+import { errorToast } from "../../../services/toaster.service";
+import { config } from "../../../config";
+import { useSelector } from "react-redux";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -20,83 +30,119 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
+  "&:last-child td, &:last-child th": {
     border: 0,
   },
 }));
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
+const Products = () => {
+  const [products, setProducts] = useState<any>({});
+  const [isLoading, setIsLoading] = useState(false);
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+  const { jwt } = useSelector((state: any) => state.auth);
 
-const Products  =()=> {
-  const [products, setProducts]=useState({});
-  const [isLoading, setIsLoading]=useState(false);
-
-  const getProducts = async()=>{
+  const getProducts = async () => {
     setIsLoading(true);
-    const resp =await getData('/product')
-    setProducts(resp.data)
+    const resp = await getData("/product");
+    setProducts(resp.data);
     setIsLoading(false);
   };
 
+  const deleteProduct = async (id: string) => {
+    try {
+      const resp =await axios.delete(`${config.SERVER_URL}/product/${id}`,{
+        headers:{
+          Authorization:`Bearer ${jwt}`,
+        },
+      });
+       const deleteHandler = products.results.filter((product: any) => {
+        return product.id !== id;
+      });
 
+      setProducts((prev: any) => {
+        return { ...prev, results: deleteHandler, count: deleteHandler.length };
+      });
+      
+    } catch (error: any) {
+      errorToast(error.response.data.error);
+      
+    }
 
-  useEffect(()=>{
+  };
+
+  useEffect(() => {
     getProducts();
-  },[]);
+  }, []);
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 100 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Image</StyledTableCell>
-            <StyledTableCell align="right">Name</StyledTableCell>
-            <StyledTableCell align="right">Price</StyledTableCell>
-            <StyledTableCell align="right">Category</StyledTableCell>
-            <StyledTableCell align="right">Brand</StyledTableCell>
-            <StyledTableCell align="right">Created At</StyledTableCell>
-            <StyledTableCell align="right">Action</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          
-            {products.results.map((product: any)=>{
-              return(
-                <StyledTableRow>
-              <StyledTableCell component="th" scope="row"> 
-              </StyledTableCell>
-              <StyledTableCell align="right"></StyledTableCell>
-              <StyledTableCell align="right"></StyledTableCell>
-              <StyledTableCell align="right"></StyledTableCell>
-              <StyledTableCell align="right"></StyledTableCell>
-              <StyledTableCell align="right"></StyledTableCell>
-              <StyledTableCell align="right"></StyledTableCell>
-            </StyledTableRow>
-              )
-            })}
-        
-        </TableBody>
-      </Table>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {products.status === "success" && (
+            <Table sx={{ minWidth: 100 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Image</StyledTableCell>
+                  <StyledTableCell align="left">Name</StyledTableCell>
+                  <StyledTableCell align="right">Price</StyledTableCell>
+                  <StyledTableCell align="right">Category</StyledTableCell>
+                  <StyledTableCell align="right">Brand</StyledTableCell>
+                  <StyledTableCell align="right">Created At</StyledTableCell>
+                  <StyledTableCell align="right">Action</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {products.results.map((product: any) => {
+                  return (
+                    <StyledTableRow key={product.id}>
+                      <StyledTableCell component="th" scope="row">
+                        <img
+                          src={product.productImage}
+                          width={"100"}
+                          height={"50"}
+                        />
+                      </StyledTableCell>
+                      <StyledTableCell align="left">
+                        {product.name}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {product.price}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {product.category}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {product.brand}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        {moment(product.createdAt).format("YYYY-MM-DD")}
+                      </StyledTableCell>
+                      <StyledTableCell align="right">
+                        <Button variant="outline-primary">
+                          <FaEdit />
+                        </Button>
+                        <Button
+                          variant="outline-danger"
+                          className="ms-2"
+                          onClick={(e) => deleteProduct(product.id)}
+                        >
+                          <AiFillDelete />
+                        </Button>
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          )}
+        </>
+      )}
     </TableContainer>
   );
-}
+};
 export default Products;
